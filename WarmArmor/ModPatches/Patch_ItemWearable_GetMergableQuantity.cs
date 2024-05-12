533293692a122d78e9fade0eaf5c26ecc57aa888
Zach2039 <sumyunguy2007@gmail.com>
2024-05-11 22:16:18 -0500

    Remove check for warmth not 0

--- WarmArmor/ModPatches/Patch_ItemWearable_GetMergableQuantity.cs
+++ WarmArmor/ModPatches/Patch_ItemWearable_GetMergableQuantity.cs
@@ -15,23 +15,19 @@ namespace WarmArmor.ModPatches
 	{
 		static bool Prefix(ItemWearable __instance, ref int __result, ItemStack sinkStack, ItemStack sourceStack, EnumMergePriority priority)
 		{
-            
-            if (priority == EnumMergePriority.DirectMerge && __instance.IsArmor)
+			
+			if (priority == EnumMergePriority.DirectMerge && __instance.IsArmor)
 			{
 				JsonObject itemAttributes = sinkStack.ItemAttributes;
 				if (itemAttributes != null && itemAttributes["warmth"].Exists)
 				{
-					JsonObject itemAttributes2 = sinkStack.ItemAttributes;
-					if (itemAttributes2 == null || itemAttributes2["warmth"].AsFloat(0f) != 0f)
-					{
-                        // Prevent armor from getting its condition repaired from repair items like twine
-                        __result = 1;
-                        return false; // skip original method
-                    }
-                }
-            }
+					// Prevent armor from getting its condition repaired from repair items like twine
+					__result = 0;
+					return false; // skip original method
+				}
+			}
 
-            return true; // resume original method
+			return true; // resume original method
 		}
 	}
 }
